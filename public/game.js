a02edfd7193813df3ee83e66811be8eec0bcc443
Mark Bieganek <mark.bieganek@telus.com>
2023-02-07 20:18:32 -0700

    Refactor Game to a class

--- public/game.js
+++ public/game.js
@@ -15,6 +15,7 @@ const canvas = getCanvasProperties(gameCanvas);
 const screenCenter = getScreenCenter();
 
 const ui = new UserInterface();
+const game = new Game();
 const goldStash = new GoldStash();
 
 let circles = [];
@@ -98,37 +99,6 @@ const configWave = {
 			this._totalGloops + (this.currentWave - 1) * this.totalGloopsMultiplier
 		);
 		return total;
-	},
-};
-
-const game = {
-	status: INITIAL_GAME_STATUS,
-	setStatus(status) {
-		const match = gameStatusTypes.filter((gameType) => gameType === status);
-		if (match.length === 0) {
-			console.log(status, "is not a valid game status😡😡😡");
-			return;
-		}
-		this.status = status;
-		if (this.status === "gameover") {
-			circles = [];
-			gloops = [];
-			towers.forEach((tower) => (tower.target = null));
-			player.setHP(0);
-		}
-		if (this.status === "active") {
-			this.reset();
-		}
-	},
-
-	reset() {
-		configWave.currentWave = INITIAL_WAVE;
-		configWave.nextWave = INITIAL_WAVE + 1;
-		goldStash.setTotal(INITIAL_GOLD_STASH_TOTAL);
-		player.setHP(INITIAL_PLAYER_HP);
-		gloops = [];
-		projectiles = [];
-		towers = [];
 	},
 };
 
